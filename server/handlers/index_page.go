26d04133f176064dfdde3a06b12f063aa6fb6a36
shunshun.lss <shunshun.lss@alibaba-inc.com>
2022-08-14 23:17:51 +0800

    rename index page

---
+++ server/handlers/index_page.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 聊天页面
+func IndexPage(c *gin.Context) {
+
+	data := gin.H{
+		"title":        "Go Live Chat",
+		"helloMessage": "hello osman",
+	}
+	c.HTML(http.StatusOK, "index.tpl", data)
+}
