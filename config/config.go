834b2e476f5473191c5da8ac406054e57df1ce1f
shunshun.lss <shunshun.lss@alibaba-inc.com>
2022-08-14 22:34:29 +0800

    add web server

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"syscall"
+
 	"github.com/spf13/viper"
 )
 
@@ -10,6 +12,20 @@ func Init() {
 
 	err := viper.ReadInConfig()
 	if err != nil {
+		panic(err)
+	}
+
+	modifyUlimit()
+}
+
+func modifyUlimit() {
+	// Increase resources limitations
+	var rLimit syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
+	}
+	rLimit.Cur = rLimit.Max
+	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
 		panic(err)
 	}
 }
--- main.go
+++ main.go
@@ -2,22 +2,13 @@ package main
 
 import (
 	"go-live-chat/config"
-	"go-live-chat/server/gateway/websocket"
-
-	log "github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
+	"go-live-chat/server"
 )
 
 func main() {
-	log.Info("gochat server starting...")
-
-	//初始化配置数据
 	config.Init()
-	log.Info("config init success")
 
-	//websocket监听端口
-	websocketPort := viper.GetInt("websocket.port")
-	websocket.Start(websocketPort)
-
-	log.Info("gochat server started at: 127.0.0.1:", websocketPort)
+	go server.StartWebSocketServer()
+	go server.StartPprofServer()
+	server.StartWebServer()
 }
